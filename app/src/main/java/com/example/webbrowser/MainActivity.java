719fa5396d12aafdac788a5bd36bc1ebba8bc012
obazehchuka <obazehchuka@gmail.com>
2024-04-10 16:28:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.webbrowser;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = findViewById(R.id.webView);
+        myWebView.loadUrl("https://x.com");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+    }
+}
